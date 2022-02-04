fa5872728b199c6e503f9c4d9b0242ff69eed84c
kota oue <kotaoue@gmail.com>
2022-02-04 23:10:33 +0900

    add draw circle function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"os"
 )
 
@@ -24,6 +25,16 @@ func main() {
 		}
 	}
 
+	circle(img, image.Point{100, 50}, 25, color.RGBA{255, 0, 0, 0})
+
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
+}
+
+func circle(img *image.RGBA, center image.Point, radius int, color color.Color) {
+	for rad := 0.0; rad < 2.0*float64(radius); rad += 0.1 {
+		x := int(float64(center.X) + float64(radius)*math.Cos(rad))
+		y := int(float64(center.Y) + float64(radius)*math.Sin(rad))
+		img.Set(x, y, color)
+	}
 }
