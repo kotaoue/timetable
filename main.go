43645905d01c0c65ee0f758417d35a7680ac55ee
kota oue <kotaoue@gmail.com>
2022-02-05 21:03:38 +0900

    make circle func

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{width, height}})
 	rect(img, 0, 0, width, height, color.White)
 
-	circle(img, image.Point{100, 50}, 25, color.RGBA{255, 0, 0, 0})
+	circle(img, (width / 2), (height / 2), 50, color.RGBA{255, 0, 0, 0})
 
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
@@ -29,10 +29,11 @@ func rect(img *image.RGBA, x, y, width, height int, color color.Color) {
 	}
 }
 
-func circle(img *image.RGBA, center image.Point, radius int, color color.Color) {
-	for r := 0.0; r < 2.0*float64(radius); r += 0.1 {
-		x := int(float64(center.X) + float64(radius)*math.Cos(r))
-		y := int(float64(center.Y) + float64(radius)*math.Sin(r))
-		img.Set(x, y, color)
+func circle(img *image.RGBA, x, y, extent int, color color.Color) {
+	r := float64(extent / 2)
+	for ang := 0.0; ang < 360.0; ang += 0.1 {
+		i := int(float64(x) + r*math.Cos(ang))
+		j := int(float64(y) + r*math.Sin(ang))
+		img.Set(i, j, color)
 	}
 }
