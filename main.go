f7ddfcd178cf275e21c7ac7780fe74fea3b2d81f
kota oue <kotaoue@gmail.com>
2022-02-11 00:28:19 +0900

    remove useless circle

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/color"
@@ -12,6 +13,15 @@ import (
 	"github.com/kotaoue/timetable/pkg/processing/palettes"
 )
 
+var (
+	width  = flag.Int("w", 400, "image width")
+	height = flag.Int("h", 400, "image height")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
 	if err := Main(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -22,18 +32,15 @@ func main() {
 func Main() error {
 	palette := palettes.ShadeGreen
 
-	width := 400
-	height := 400
-	size := int(math.Max(float64(width), float64(height)) / 2)
+	size := int(math.Max(float64(*width), float64(*height)) / 2)
 
-	img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{width, height}})
+	img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{*width, *height}})
 	prc := processing.NewProcessing(processing.Config{Image: img})
 
 	prc.Fill(&color.RGBA{255, 255, 255, 255})
-	prc.Rect(0, 0, width, height)
+	prc.Rect(0, 0, *width, *height)
 
 	prc.Stroke(&palette[0])
-	prc.Circle((width / 2), (height / 2), size)
 
 	hours := 24
 	ang := 360 / hours
@@ -41,7 +48,7 @@ func Main() error {
 		prc.Fill(&palette[i%4])
 
 		fmt.Printf("i:%d start:%g end:%g\n", i, float64(i*ang), float64((i+1)*ang))
-		prc.Pie((width / 2), (height / 2), size, float64(i*ang), float64((i+1)*ang))
+		prc.Pie((*width / 2), (*height / 2), size, float64(i*ang), float64((i+1)*ang))
 	}
 
 	f, err := os.Create("image.png")
